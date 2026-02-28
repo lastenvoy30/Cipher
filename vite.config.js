b7181784fbca927c1a9ae3469c0e44b5298a9075
Mirza Isfand <mirzaisfand321@gmail.com>
2026-02-28 20:54:36 +0530

    lame error fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     dedupe: ['react', 'react-dom'],
-  base: process.env.VITE_BASE_PATH || "Cipher",
+  base: process.env.VITE_BASE_PATH || "/Cipher",
 
   },
 })
