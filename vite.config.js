9357b8e874399564a513e21efcda1803b9955e32
Mirza Isfand <mirzaisfand321@gmail.com>
2026-02-28 20:45:26 +0530

    deploying on vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/Cipher"
   resolve: {
     dedupe: ['react', 'react-dom'],
   },
